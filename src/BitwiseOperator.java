c2b037917075442d74b7cdb33141171ed1962975
A Supriya <A Supriya@DESKTOP-GGBJ3LV>
2020-03-16 00:00:24 +0530

    Commit

---
+++ src/BitwiseOperator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A Supriya
+ */
+public class BitwiseOperator {
+    public static void main(String args[])
+ {
+     int a=25;//11001
+     int b=15;//01111
+     int c=a & b;
+     int d=a | b;
+     System.out.println(c);   
+     System.out.println(d); 
+}
+} 
---
+++ src/PreIncrement.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A Supriya
+ */
+public class PreIncrement {
+    public static void main(String args[])
+ {
+     int i=5;
+     int j=++i;
+     System.out.println(j + " : " + i);   
+}
+}
