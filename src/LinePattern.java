3f0e1362401c987049a0bdbe696b401f2f1b57d4
A Supriya <A Supriya@DESKTOP-GGBJ3LV>
2020-03-16 22:56:57 +0530

    Commit

---
+++ src/LinePattern.java
@@ -0,0 +1,10 @@
+public class LinePattern {
+   public static void main(String args[])
+   {
+    for(int i=1;i<=5;i++)
+    {
+    System.out.print("* ");
+    } 
+        System.out.println();
+   }
+}
